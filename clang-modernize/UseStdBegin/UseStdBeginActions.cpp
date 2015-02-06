8f6b4a4237f7cae6519208cccc4d037b86399724
xinhuang <patz.hwang@gmail.com>
2015-02-06 23:22:14 +0800

    remove const_cast and use std::cbegin

--- clang-modernize/UseStdBegin/UseStdBeginActions.cpp
+++ clang-modernize/UseStdBegin/UseStdBeginActions.cpp
@@ -11,12 +11,40 @@ using namespace clang::ast_matchers;
 
 #include <string>
 
-std::string getCalleeName(const SourceManager &SM, const Expr &E) {
+namespace {
+
+std::string getExprText(const SourceManager &SM, const Expr &E) {
+  const auto &LocStart = E.getLocStart();
+  const auto &LocEnd = E.getLocEnd();
+  const char *CharStart = SM.getCharacterData(LocStart);
+  const char *CharEnd = SM.getCharacterData(LocEnd);
+  return std::string(CharStart, CharEnd + 1);
+}
+
+std::string getExprText(const SourceManager &SM, const MemberExpr &E) {
   const auto &LocStart = E.getLocStart();
   const auto &LocEnd = E.getLocEnd();
   const char *CharStart = SM.getCharacterData(LocStart);
   const char *CharEnd = SM.getCharacterData(LocEnd);
   return std::string(CharStart, CharEnd - 1);
+}
+
+const Expr *ignoreConstCasts(const Expr *ME) {
+  const Expr *E = ME;
+  while (true) {
+    if (const auto *P = dyn_cast<const CXXConstCastExpr>(E)) {
+      E = P->getSubExpr();
+      continue;
+    }
+    if (const auto *P = dyn_cast<const ParenExpr>(E)) {
+      if (P == ignoreConstCasts(P->getSubExpr())) {
+        return E;
+      }
+      E = P;
+    }
+    return E;
+  }
+}
 }
 
 void MemberBeginReplacer::run(const MatchFinder::MatchResult &Result) {
@@ -28,11 +56,15 @@ void MemberBeginReplacer::run(const MatchFinder::MatchResult &Result) {
   if (Member == nullptr) {
     return;
   }
-  const auto &RefName = getCalleeName(SM, *Member);
+  auto RefName = getExprText(SM, *Member);
   bool IsConst = Member->getBase()->getType().isConstQualified();
 
   auto MethodName = MemberCall->getMethodDecl()->getNameAsString();
-  MethodName = IsConst ? "c" + MethodName : MethodName;
+  if (IsConst) {
+    MethodName = "c" + MethodName;
+    const auto *NonConstExpr = ignoreConstCasts(Member->getBase());
+    RefName = getExprText(SM, *NonConstExpr);
+  }
   const auto StartLoc = SM.getSpellingLoc(MemberCall->getLocStart());
   const auto EndLoc = SM.getSpellingLoc(MemberCall->getLocEnd());
 
